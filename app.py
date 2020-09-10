454dce173a19cede2866880a47f672e02d67c5cb
Aditya Shah <aditya7395@gmail.com>
2020-09-10 16:16:17 -0700

    Added file upload functionality and url redirect to files and shortened urls

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = 'hasdfh123hkjhadsfj1341'
@@ -22,9 +23,27 @@ def your_url():
             flash('That short name has already been taken. Please select another name')
             return redirect(url_for('home'))
 
-        urls[request.form['code']] = {'url': request.form['url']}
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url': request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save('/Users/adshah/Documents/Python/URL-Shortener/static/user_files/' + full_name)
+            urls[request.form['code']] = {'file': full_name}
+
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
     else:
         return redirect(url_for('home'))
+
+@app.route('/<string:code>') #Look for any variable and put it in the variable code
+def redirect_to_url(code):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
+                else:
+                    return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
